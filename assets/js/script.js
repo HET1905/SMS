249bff2c5e386368f74cf36a4a7cb6a0ddcf9a4a
Hetal Patel <nphetal1905@gmail.com>
2020-06-21 21:57:43 -0400

    dynamic for first two

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,44 @@
 $(document).ready(function () {
-  $("div#projectDisplay .card").click(function () {
-    var myID = $(this).attr("Id");
-    alert(myID);
+  var myID = $(this).attr("Id");
+  // alert(myID);
 
-    function clickOnClass(className) {
-      $(".corporate").click(function () {
-        $.getJSON("data.json", function (data) {
-          var items = [];
-          $.each(data, function (key, val1) {
-            $.each(val1, function (index) {
-              console.log(val1[index]);
-              items.push(`<li> ${val1[index]} </li>`);
-            });
-          });
+  // myID.click(clickOnClass(myID));
 
-          $(".modal-body").empty();
-          $("<ul/>", {
-            // class: "my-new-list",
-            html: items,
-          }).appendTo(".modal-body");
-          // $(".modal-body").html(myHeading + ulList);
-          $(".modal").modal("show");
+  $(".corporate").click(function () {
+    $.getJSON("data.json", function (data) {
+      var items = [];
+      $.each(data, function (i, key, val1) {
+        $.each(key["corporate"], function (index) {
+          items.push(`<li> ${key["corporate"][index]} </li>`);
         });
       });
-    }
+
+      $(".modal-body").empty();
+      $("<ul/>", {
+        // class: "my-new-list",
+        html: items,
+      }).appendTo(".modal-body");
+      // $(".modal-body").html(myHeading + ulList);
+      $(".modal").modal("show");
+    });
+  });
+
+  $("#NGO").click(function () {
+    $.getJSON("data.json", function (data) {
+      var items = [];
+      $.each(data, function (i, key, val1) {
+        $.each(key["NGO"], function (index) {
+          items.push(`<li> ${key["NGO"][index]} </li>`);
+        });
+      });
+
+      $(".modal-body").empty();
+      $("<ul/>", {
+        // class: "my-new-list",
+        html: items,
+      }).appendTo(".modal-body");
+      // $(".modal-body").html(myHeading + ulList);
+      $(".modal").modal("show");
+    });
   });
 });
