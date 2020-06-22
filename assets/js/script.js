fbcb152a072d0ec991468f442c2ec00e41829930
Hetal Patel <nphetal1905@gmail.com>
2020-06-21 22:35:43 -0400

    first row finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,9 @@ $(document).ready(function () {
         });
       });
 
+      $(".modal-title").empty();
+      $(".modal-title").append("Corporate");
+
       $(".modal-body").empty();
       $("<ul/>", {
         // class: "my-new-list",
@@ -31,6 +34,52 @@ $(document).ready(function () {
           items.push(`<li> ${key["NGO"][index]} </li>`);
         });
       });
+
+      $(".modal-title").empty();
+      $(".modal-title").append("NGO and Social bodies");
+      $(".modal-body").empty();
+      $("<ul/>", {
+        // class: "my-new-list",
+        html: items,
+      }).appendTo(".modal-body");
+      // $(".modal-body").html(myHeading + ulList);
+      $(".modal").modal("show");
+    });
+  });
+
+  $("#GovtDept").click(function () {
+    $.getJSON("data.json", function (data) {
+      var items = [];
+      $.each(data, function (i, key, val1) {
+        $.each(key["GovtDept"], function (index) {
+          items.push(`<li> ${key["GovtDept"][index]} </li>`);
+        });
+      });
+
+      $(".modal-title").empty();
+      $(".modal-title").append(" Govt.Departments");
+
+      $(".modal-body").empty();
+      $("<ul/>", {
+        // class: "my-new-list",
+        html: items,
+      }).appendTo(".modal-body");
+      // $(".modal-body").html(myHeading + ulList);
+      $(".modal").modal("show");
+    });
+  });
+
+  $("#SHGs").click(function () {
+    $.getJSON("data.json", function (data) {
+      var items = [];
+      $.each(data, function (i, key, val1) {
+        $.each(key["SHG"], function (index) {
+          items.push(`<li> ${key["SHG"][index]} </li>`);
+        });
+      });
+
+      $(".modal-title").empty();
+      $(".modal-title").append("SHGs(Self Help Groups)");
 
       $(".modal-body").empty();
       $("<ul/>", {
